4858e7379927acc2e9571624491d0afab6fafe70
vinayBagde <vinaybagde0@gmail.com>
2024-08-30 16:53:18 +0530

    create lottery ticket component

---
+++ src/components/lotteryTicket/LotteryTicket.jsx
@@ -0,0 +1,35 @@
+import "./LotteryTicket.css";
+import { useState } from "react";
+export default function LotteryTicket() {
+  let [num, setNum] = useState(0);
+  let [sum, setSum] = useState(0);
+  let generateNumber = () => {
+    setNum(() => {
+      return Math.floor(Math.random() * 899 + 100);
+    });
+
+   
+    setSum(() => {
+      let temp = 0;
+      for (let i = 0; i <= 3; i++) {
+        temp += num % 10;
+        num = Math.floor(num / 10);
+      }
+      return temp;
+    });
+    
+  };
+  return (
+    <>
+      <div id="wonTicket">
+        {sum == 15 ? <h1>Lottery' Congratulation, you won!</h1> : null}
+      </div>
+      <div id="ticket-box">
+        <h1 style={{ padding: "0 10px 0 10px" }}>Lottery</h1>
+        <h3>Lottery Ticket = {num}</h3>
+        <h5>Sum : {sum}</h5>
+        <button onClick={generateNumber}>click</button>
+      </div>
+    </>
+  );
+}
