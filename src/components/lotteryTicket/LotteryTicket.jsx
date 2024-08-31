630d955f5961dddbcdc88f8438a8e905a4906cc4
vinayBagde <vinaybagde0@gmail.com>
2024-09-01 02:20:13 +0530

    solve sum state problem

--- src/components/lotteryTicket/LotteryTicket.jsx
+++ src/components/lotteryTicket/LotteryTicket.jsx
@@ -3,20 +3,21 @@ import { useState } from "react";
 export default function LotteryTicket() {
   let [num, setNum] = useState(0);
   let [sum, setSum] = useState(0);
+
+
   let generateNumber = () => {
     setNum(() => {
       return Math.floor(Math.random() * 899 + 100);
     });
+     
 
-   
-    setSum(() => {
-      let temp = 0;
-      for (let i = 0; i <= 3; i++) {
-        temp += num % 10;
-        num = Math.floor(num / 10);
-      }
-      return temp;
-    });
+    let temp = 0;
+     for (let i = 0; i <= 3; i++) {
+       temp += num % 10;
+       num = Math.floor(num / 10);
+     }
+   // why this function is not working
+    setSum(temp);
     
   };
   return (
@@ -28,7 +29,7 @@ export default function LotteryTicket() {
         <h1 style={{ padding: "0 10px 0 10px" }}>Lottery</h1>
         <h3>Lottery Ticket = {num}</h3>
         <h5>Sum : {sum}</h5>
-        <button onClick={generateNumber}>click</button>
+        <button onClick={generateNumber}>Get new ticket</button>
       </div>
     </>
   );
